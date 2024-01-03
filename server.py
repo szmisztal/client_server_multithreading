4c02e101078c86511751fd7b01b5e4df5e16b66d
szmisztal <sz.misztal@gmail.com>
2024-01-03 12:19:29 +0100

    delete connection_pool atribute

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import socket as s
-from connection_pool import ConnectionPool
 from variables import HOST, PORT, INTERNET_ADDRESS_FAMILY, SOCKET_TYPE, BUFFER, encode_format
 from data_utils import DataUtils
 
@@ -12,7 +11,6 @@ class Server():
         self.SOCKET_TYPE = SOCKET_TYPE
         self.BUFFER = BUFFER
         self.data_utils = DataUtils()
-        self.connection_pool = ConnectionPool(self, 10, 100)
         self.encode_format = encode_format
         self.is_running = True
 
@@ -25,14 +23,15 @@ class Server():
                 while self.is_running:
                     client_socket, address = server_socket.accept()
                     try:
-                        client_handler = self.connection_pool.get_connection(client_socket, address)
-                        client_handler.run()
-                        if len(self.connection_pool.connections_in_use_list) >= 100:
-                            connections_to_close = list(self.connection_pool.connections_in_use_list)
-                            for handler in connections_to_close:
-                                handler.stop_message()
-                                self.connection_pool.release_connection(handler)
-                                self.is_running = False
+                        pass
+                        # client_handler = self.connection_pool.get_connection(client_socket, address)
+                        # client_handler.run()
+                        # if len(self.connection_pool.connections_in_use_list) >= 100:
+                        #     connections_to_close = list(self.connection_pool.connections_in_use_list)
+                        #     for handler in connections_to_close:
+                        #         handler.stop_message()
+                        #         self.connection_pool.release_connection(handler)
+                        #         self.is_running = False
                     except Exception as e:
                         print(f"Error handling client {address}: {e}")
             except KeyboardInterrupt:
