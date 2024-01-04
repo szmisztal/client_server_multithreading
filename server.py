ea57cb356e673aeb23da497797d1496d89e59a0d
szmisztal <sz.misztal@gmail.com>
2024-01-04 10:37:16 +0100

    add socket.timeout(5) for every client and change thread service from _thread module to threading module

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socket as s
 import threading
-from _thread import *
 from variables import HOST, PORT, INTERNET_ADDRESS_FAMILY, SOCKET_TYPE, BUFFER, encode_format
 from data_utils import DataUtils
 
@@ -18,22 +17,27 @@ class Server():
         self.lock = threading.Lock()
         self.clients_list = []
 
-    def client_thread(self, client):
+    def client_thread(self, client_socket):
         message = {"OK": len(self.clients_list)}
         message_json = self.data_utils.serialize_to_json(message)
-        client.sendall(message_json)
+        client_socket.sendall(message_json)
 
     def send_ping_message_and_shut_down_server(self):
         message = "PING"
         message_json = self.data_utils.serialize_to_json(message)
         for client_socket in self.clients_list:
-            self.lock.acquire()
-            client_socket.sendall(message_json)
-            response_json = client_socket.recv(BUFFER)
-            response = self.data_utils.deserialize_json(response_json)
-            print(response)
-            client_socket.close()
-            self.lock.release()
+            client_socket.settimeout(5)
+            try:
+                self.lock.acquire()
+                client_socket.sendall(message_json)
+                response_json = client_socket.recv(BUFFER)
+                response = self.data_utils.deserialize_json(response_json)
+                print(response)
+                client_socket.close()
+                self.lock.release()
+            except client_socket.timeout as e:
+                print(f"Error: {e}")
+                client_socket.close()
 
     def start(self):
         with s.socket(self.INTERNET_ADDRESS_FAMILY, self.SOCKET_TYPE) as server_socket:
@@ -46,13 +50,14 @@ class Server():
                     try:
                         self.lock.acquire()
                         self.clients_list.append(client_socket)
-                        start_new_thread(self.client_thread, (client_socket,))
+                        thread = threading.Thread(target = self.client_thread, args = (client_socket, ))
+                        thread.start()
                         self.lock.release()
                         if len(self.clients_list) >= 100:
                             self.send_ping_message_and_shut_down_server()
                             self.is_running = False
                     except Exception as e:
-                        print(f"Error handling client {address}: {e}")
+                        print(f"Error: {e}")
             except KeyboardInterrupt:
                 print("Server stopped by user.")
             finally:
